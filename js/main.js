fc53912b808adfcc55dac6a6dfb66f11f645e845
DinhDoanNguyen <nguyendinhdoan.09@gmail.com>
2021-02-02 22:49:52 +0700

    update

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var fullName = "Nguyen Dinh Doan";
+
+// alert(fullName);
+// console.log(fullName);
+// confirm("Xác nhận bạn đủ 18 tuổi");
+// prompt("Bạn bao nhiêu tủi");
+// setTimeout(function() {
+//     alert("Hello Đoàn")
+// }, 1000);
+// setInterval(() => {
+//    console.log("Hello" + Math.random()) 
+// }, 1000);
+
+// var a = 4;
+// var b = 5;
+// if (a == b) {
+//     alert("Đúng");
+// } else {
+//     alert("Sai");
+// }
+
+// var a = 5;
+// console.log(a);
+// ++a;
+// console.log(a);
+
+var firsName = "Doan";
+var lastName = "Nguyen";
+var fullName = "Nguyen Dinh " + firsName;
+
+console.log(firsName + " " + lastName)
+
+console.log("Nguyen Dinh " + firsName)
+
+console.error(fullName);
